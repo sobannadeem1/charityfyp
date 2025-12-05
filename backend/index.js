8522e844f52cc7c1e1d046d1a4bcb88526b1ac3f
sobannadeem1 <soban312004@gmail.com>
2025-12-05 17:39:25 +0500

    cors issue fixed

--- backend/index.js
+++ backend/index.js
@@ -13,13 +13,18 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(
-  cors({
-    origin: ["https://charityfyp.vercel.app", "http://localhost:5173"],
+app.use(cors());
+app.options("*", cors());
 
-    credentials: true,
-  })
-);
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (origin) res.setHeader("Access-Control-Allow-Origin", origin);
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") return res.sendStatus(200);
+  next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
