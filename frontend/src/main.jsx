22a0e1321e396ff531bddda4a1f831dd59b61628
sobannadeem1 <soban312004@gmail.com>
2025-12-05 17:43:31 +0500

    speedinsights added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -18,6 +18,7 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <NotificationProvider>
       <QueryClientProvider client={queryClient}>
+        <SpeedInsights />
         <App />
       </QueryClientProvider>
     </NotificationProvider>
